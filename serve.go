0660166d509db05f56bc21bb8491f9b8baf38dbe
Jeff Hodges <jeff@somethingsimilar.com>
2015-02-23 22:45:27 -0800

    add ability to specify response headers

--- serve.go
+++ serve.go
@@ -8,10 +8,12 @@ import (
 	"net/http/httputil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var (
 	addr    = flag.String("addr", "localhost:10000", "address to serve from")
+	headers stringSlice
 	quiet   = flag.Bool("q", false, "log nothing")
 	verbose = flag.Bool("v", false, "verbose logging, dumping requests")
 )
@@ -28,6 +30,7 @@ func die(sfmt string, objs ...interface{}) {
 }
 
 func main() {
+	flag.Var(&headers, "H", "header to set on the response, may be specified multiple times")
 	flag.Usage = usage
 	flag.Parse()
 	if len(flag.Args()) > 1 {
@@ -62,7 +65,14 @@ func main() {
 	if !fi.IsDir() {
 		die("'%s' was not a directory", path)
 	}
-	h := http.FileServer(http.Dir(path))
+
+	var h http.Handler
+
+	h = &headerHandler{
+		hs:    toHeader(headers),
+		inner: http.FileServer(http.Dir(path)),
+	}
+
 	if !*quiet {
 		log.Printf("Serving %s on %s", path, *addr)
 	}
@@ -72,7 +82,22 @@ func main() {
 	} else if !*quiet {
 		h = &verboseHandler{h}
 	}
+
 	log.Fatal(http.ListenAndServe(*addr, h))
+}
+
+type headerHandler struct {
+	hs    http.Header
+	inner http.Handler
+}
+
+func (hh *headerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for k, vs := range hh.hs {
+		for _, v := range vs {
+			w.Header().Add(k, v)
+		}
+	}
+	hh.inner.ServeHTTP(w, r)
 }
 
 type verboseHandler struct {
@@ -97,4 +122,29 @@ func (rh *requestDumpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		log.Println("Unable to dump request: %s", err)
 	}
 	rh.inner.ServeHTTP(w, r)
+}
+
+type stringSlice []string
+
+func (s *stringSlice) String() string {
+	return fmt.Sprintf("%s", *s)
+}
+
+func (s *stringSlice) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+func toHeader(s stringSlice) http.Header {
+	h := make(http.Header)
+	for _, raw := range s {
+		split := strings.SplitN(raw, ":", 2)
+		key := split[0]
+		val := ""
+		if len(split) == 2 {
+			val = split[1]
+		}
+		h.Add(key, val)
+	}
+	return h
 }
